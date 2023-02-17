21cf66fbd769909de578855fa3881778c4e97911
kanugirba <125104310+kanugirba@users.noreply.github.com>
2023-02-17 10:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import time
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from utils import preprocess_image, build_model, predict, create_opencv_image_from_stringio
+from utils import preprocess_image, build_model, predict
 import cv2
 import matplotlib.pyplot as plt
+from PIL import Image
 
 model_path = "./fold-4.h5" # replace this by downloading the model file from the drive link attached in the Readme.md file
 model = build_model(ef=4)
@@ -16,13 +17,12 @@ model.load_weights(model_path)
 st.write("Diabetic retinopathy is a serious eye condition that can lead to vision loss. Early detection and treatment are crucial for preventing vision loss. However, it is important to note that this software is not a substitute for a professional medical opinion. If you have any concerns about your eye health, please consult a doctor for a comprehensive eye exam and proper diagnosis.")
 uploaded_file = st.file_uploader("Upload the Image")
 if st.button('Predict'):
-    img = create_opencv_image_from_stringio(uploaded_file)
-    img = preprocess_image(img, crop=True, blur = True, sigmaX=10)
+    img = Image.open(uploaded_file)
+    img = preprocess_image(np.array(img), crop=True, blur = True, sigmaX=10)
+    image = tf.cast(img, tf.float32) / 255.0
+    image = tf.reshape(image, [512,512, 3])
     st.image(img)
-    data = tf.keras.applications.efficientnet.preprocess_input(
-        img, data_format=None
-    )
-    data = np.expand_dims(data, axis=0)
+    data = np.expand_dims(image, axis=0)
     output , proba = predict(model,data)
     classes = ["No","Mild","Moderate","Severe","Proliferative"]
     st.write(f"The uploaded image is having {classes[output[0]]} Diabetic Retinopathy.")
@@ -31,6 +31,7 @@ if st.button('Predict'):
     plt.yticks(range(len(proba[0])),classes)
     plt.title('Probabilities of Each Class')
     st.pyplot(fig)
+
 
 
 
